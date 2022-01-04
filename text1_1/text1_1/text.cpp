2b0cab25a43dc9b6c003d29bb063d44d0963865a
2856672598 <2856672598@qq.com>
2022-01-04 20:51:14 +0800

    练习

--- text1_1/text1_1/text.cpp
+++ text1_1/text1_1/text.cpp
@@ -283,37 +283,287 @@
 //}
 
 
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+////class Solution {
+////public:
+////	int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
+////	{
+////		for (int i = 0; i < gas.size(); i++)
+////		{
+////			int over = gas[i];
+////			if (gas[i] < cost[i])
+////				continue;
+////			over = gas[i] - cost[i];
+////			int index = (i + 1) % gas.size();
+////			while (index != i)
+////			{
+////				over = over - cost[index] + gas[index];
+////				if (over < 0)
+////					break;
+////				index = (index + 1) % gas.size();
+////			}
+////
+////			if (index == i)
+////				return i;
+////		}
+////		return -1;
+////	}
+////};
+//
+//
+//class Solution {
+//public:
+//	int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
+//	{
+//		int curSum = 0;
+//		int totalSum = 0;
+//		int statu = 0;
+//		for (int i = 0; i < gas.size(); i++)
+//		{
+//			curSum += gas[i] - cost[i];
+//			totalSum += gas[i] - cost[i];
+//			if (curSum < 0)
+//			{
+//				statu = i + 1;
+//				curSum = 0;
+//			}
+//		}
+//
+//		if (totalSum < 0)
+//			return -1;
+//		return statu;
+//	}
+//};
+//
+//
+//int main()
+//{
+//	vector<int>gas{ 2,3,4 };
+//	vector<int>cost{ 3,4,3 };
+//	cout << Solution().canCompleteCircuit(gas, cost) << endl;
+//	return 0;
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//
+//class Solution
+//{
+//public:
+//	string dayOfTheWeek(int day, int month, int year)
+//	{
+//		string week[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+//		int count = 0;
+//		for (int i = 1971; i < year; i++)
+//		{
+//			if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+//				count += 366;
+//			else
+//				count += 365;
+//		}
+//		int nums[13] = { 0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//		for (int i = 1; i < month; i++)
+//		{
+//			count += nums[i];
+//			if ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && i == 2)
+//				count += 1;
+//		}
+//		count += day;
+//		return week[((count % 7) + 4) % 7];
+//	}
+//};
+//
+//int main()
+//{
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//
+//class Solution
+//{
+//public:
+//	int balancedStringSplit(string s)
+//	{
+//		int count = 0;
+//		int flag = 0;
+//		for (auto e : s)
+//		{
+//			if (e == 'L')
+//				flag++;
+//			else
+//				flag--;
+//
+//			if (flag == 0)
+//				count++;
+//		}
+//		return count;
+//	}
+//};
+//
+//int main()
+//{
+//	string s = { "RLRRLLRLRL" };
+//	cout << Solution().balancedStringSplit(s);
+//	return 0;
+//}
+
+//#include<iostream>
+//#include<cstdlib>
+//#include<string>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int maximum69Number(int num)
+//	{
+//		string s = to_string(num);
+//		for (auto& e : s)
+//		{
+//			if (e == '6')
+//			{
+//				e = '9';
+//				break;
+//			}
+//		}
+//		return stoi(s);
+//	}
+//};
+//
+//int main()
+//{
+//	int num = 9996;
+//	cout << Solution().maximum69Number(num) << endl;
+//	return 0;
+//}
+
+////1827. 最少操作使数组递增
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int minOperations(vector<int>& nums)
+//	{
+//		int count = 0;
+//		for (int i = 1; i < (int)nums.size(); i++)
+//		{
+//			if (nums[i] <= nums[i-1])
+//			{
+//				int tmp = nums[i - 1] + 1;
+//				count += tmp - nums[i];
+//				nums[i] = tmp;
+//			}
+//		}
+//		return count;
+//	}
+//};
+//
+//int main()
+//{
+//	vector<int>nums{ 1,5,2,4,1 };
+//	cout << Solution().minOperations(nums) << endl;
+//	return  0;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//#include<algorithm>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int minTimeToType(string word)
+//	{
+//		int count = 0;
+//		int cur = 'a', prev = 'a';
+//		for (auto e : word)
+//		{
+//			cur = e;
+//			count += 1 + min(abs(cur - prev), 26 - abs(cur - prev));
+//			prev = cur;
+//		}
+//		return count;
+//	}
+//};
+//
+//int main()
+//{
+//	string word{ "abc" };
+//	cout << Solution().minTimeToType(word) << endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int maxIncreaseKeepingSkyline(vector<vector<int>>& grid)
+//	{
+//		vector<int>row, col;
+//		row.resize(grid.size()), col.reserve(grid.size());
+//		for (int i = 0; i < (int)grid.size(); i++)
+//		{
+//			for (int j = 0; j < (int)grid[i].size(); j++)
+//			{
+//				row[i] = max(grid[i][j], row[i]);
+//				col[j] = max(grid[i][j], col[j]);
+//			}
+//		}
+//		int sum = 0;
+//		for (int i = 0; i < (int)grid.size(); i++)
+//		{
+//			for (int j = 0; j < (int)grid[i].size(); j++)
+//			{
+//				sum += min(row[i], col[j]) - grid[i][j];
+//			}
+//		}
+//		return sum;
+//	}
+//};
+//int main()
+//{
+//	return 0;
+//}
+
+
+//1877. 数组中最大数对和的最小值
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 class Solution {
 public:
-	int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
+	int minPairSum(vector<int>& nums)
 	{
-		for (int i = 0; i < gas.size(); i++)
+		sort(nums.begin(), nums.end());
+		int left = 0, right = nums.size() - 1;
+		int ret = nums[left] + nums[right];
+		while (left < right)
 		{
-			int over = gas[i];
-			if (gas[i] < cost[i])
-				continue;
-			over = gas[i] - cost[i];
-			int index = (i + 1) % gas.size();
-			while (index != i)
-			{
-				over += cost[index] - gas[index];
-				index = (index + 1) % gas.size();
-				if (over < 0)
-					break;
-			}
-
-			if (index == i)
-				return i;
+			ret = max(ret, nums[left++] + nums[right--]);
 		}
-		return -1;
+		return ret;
 	}
 };
 
 int main()
 {
+	vector<int>nums = { 3,5,4,2,4,6 };
+	cout << Solution().minPairSum(nums) << endl;
 	return 0;
 }
