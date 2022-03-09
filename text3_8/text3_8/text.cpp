c1d7c0dd5c1ac67389938d4d0932b09bf4e63e94
2856672598 <2856672598@qq.com>
2022-03-09 21:22:57 +0800

    3/9

--- text3_8/text3_8/text.cpp
+++ text3_8/text3_8/text.cpp
@@ -83,3 +83,176 @@
 //	return 0;
 //}
 
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int pivotIndex(vector<int>& nums) {
+//		vector<int>sum(nums.size(), 0);
+//		sum[0] = nums[0];
+//		for (int i = 1; i < nums.size(); i++)
+//		{
+//			sum[i] = sum[i - 1] + nums[i];
+//		}
+//		int prev = 0;
+//		for (int i = (nums.size()) - 1; i >= 0; i--)
+//		{
+//			sum[i] -= prev + nums[i];
+//			prev += nums[i];
+//		}
+//		auto pos = find(sum.begin(), sum.end(), 0);
+//		return pos == sum.end() ? -1 : pos - sum.begin();
+//	}
+//};
+//
+//int main()
+//{
+//	vector<int>nums{ 1,7,3,6,5,6 };
+//	cout << Solution().pivotIndex(nums);
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//
+//struct ListNode {
+//	int val;
+//	ListNode *next;
+//	ListNode(int x) : val(x), next(NULL) {}
+//};
+//
+//class Solution {
+//public:
+//	void deleteNode(ListNode* node) {
+//		node->val = node->next->val;
+//		node->next = node->next->next;
+//	}
+//};
+
+//#include<iostream>
+//#include<vector>
+//#include<queue>
+//using namespace std;
+//class AnimalShelf {
+//private :
+//	queue<int>cat;
+//	queue<int>dog;
+//public:
+//	AnimalShelf() {
+//
+//	}
+//
+//	void enqueue(vector<int> animal) {
+//		if (animal[1] == 0)
+//			cat.push(animal[0]);
+//		else
+//			dog.push(animal[0]);
+//	}
+//
+//	vector<int> dequeueAny() {
+//		if (dog.empty())
+//			return dequeueCat();
+//		else if (cat.empty())
+//			return dequeueDog();
+//		else if (cat.front() < dog.front())
+//			return dequeueCat();
+//		else
+//			return dequeueDog();
+//	}
+//
+//	vector<int> dequeueDog() {
+//		if (dog.empty())
+//			return { -1,-1 };
+//		int num = dog.front();
+//		dog.pop();
+//		return { num,1 };
+//	}
+//
+//	vector<int> dequeueCat() {
+//		if (cat.empty())
+//			return { -1,-1 };
+//		int num = cat.front();
+//		cat.pop();
+//		return { num,0 };
+//	}
+//};
+
+
+//#include<iostream>
+//#include<string>
+//#include<algorithm>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int lengthOfLongestSubstring(string s) {
+//		int arr[256] = { 0 };
+//		int left = 0, right = 0;
+//		int ret = 0;
+//		while (right < s.size())
+//		{
+//			arr[s[right]]++;
+//			while (left < right &&arr[s[right]] > 1)
+//			{
+//				arr[s[left++]]--;
+//			}
+//			ret = max(ret, right - left + 1);
+//			right++;
+//		}
+//		return ret;
+//	}
+//};
+//
+//int main()
+//{
+//	string s{ "abcabcbb" };
+//	cout << Solution().lengthOfLongestSubstring(s);
+//	return 0;
+//}
+
+#include<iostream>
+#include <unordered_map>
+#include<vector>
+#include <ctime>
+using namespace std;
+
+class RandomizedSet {
+private:
+	unordered_map<int,int> s;
+	vector<int>arr;
+public:
+	/** Initialize your data structure here. */
+	RandomizedSet() {
+		srand((unsigned)time(nullptr));
+	}
+
+	/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+	bool insert(int val) {
+		if (s.find(val) != s.end())
+			return false;
+		arr.push_back(val);
+		s.insert(make_pair(val, arr.size() - 1));
+		return true;
+	}
+
+	/** Removes a value from the set. Returns true if the set contained the specified element. */
+	bool remove(int val) {
+		if (s.find(val) == s.end())
+			return false;
+		int tmp = arr[arr.size() - 1];
+		swap(arr[s[val]], arr[arr.size() - 1]);
+		s[tmp] = s[val];
+		s.erase(val);
+		arr.pop_back();
+		return true;
+	}
+
+	/** Get a random element from the set. */
+	int getRandom() {
+		int pos = rand() % arr.size();
+		return arr[pos];
+	}
+};
